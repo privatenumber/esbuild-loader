adbddb636c41aa0703ae2e836b1dd863901fb23e
Mathew Kurian <mwkurian@gmail.com>
2020-07-20 11:37:27 -0500

    Added support for sourcemaps, error for invalid file types, reverted change for loader type

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ const path = require('path')
 const esbuild = require('esbuild')
 const { getOptions } = require('loader-utils')
 
+const FILE_EXTS = ['.json', '.js', '.tsx', '.jsx', '.ts', '.text', '.base64']
+
 const getLoader = (ext) => {
+  if (!FILE_EXTS.includes(ext)) {
+    throw Error(
+      '[esbuild-loader] Please ensure the `rule.test` attribute of this loader filters for the following file types: ' +
+        FILE_EXTS.join(',')
+    )
+  }
+
   if (ext === '.json') {
     return 'json'
   }
-  return 'tsx'
+  return ext.slice(1)
 }
 
 module.exports = async function (source) {
@@ -32,6 +41,7 @@ module.exports = async function (source) {
       jsxFactory: options.jsxFactory,
       jsxFragment: options.jsxFragment,
       sourcemap: options.sourceMap,
+      sourcefile: options.sourceMap ? this.resourcePath : null,
     })
     done(null, result.js, result.jsSourceMap)
   } catch (err) {
