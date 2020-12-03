8dd95bbcce4b041469d9d7e2b33bc2b2f9a3a2c7
Hiroki Osame <hiroki.osame@gmail.com>
2020-12-02 22:45:08 -0500

    style: use Except over Omit

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,8 +1,10 @@
 import {Service, TransformOptions} from 'esbuild';
 import {Compiler as Webpack4Compiler} from 'webpack';
+import {Except} from 'type-fest';
 
 export type Compiler = Webpack4Compiler & {
 	$esbuildService?: Service;
-}
-export type LoaderOptions = Omit<TransformOptions, 'sourcemap' | 'sourcefile'>;
-export type MinifyPluginOptions = Omit<TransformOptions, 'sourcefile'>;
+};
+
+export type LoaderOptions = Except<TransformOptions, 'sourcemap' | 'sourcefile'>;
+export type MinifyPluginOptions = Except<TransformOptions, 'sourcefile'>;
