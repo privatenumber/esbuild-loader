84cfe8d2d050c57d7c2ab896d864e77a9b97aacc
hiroki osame <hiroki.osame@gmail.com>
2021-12-21 03:15:49 -0500

    ci: lint prs (#234)

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@ import esbuildLoader from './loader';
 import ESBuildPlugin from './plugin';
 import ESBuildMinifyPlugin from './minify-plugin';
 
+/* eslint-disable unicorn/prefer-export-from */
 export default esbuildLoader;
 export { ESBuildPlugin, ESBuildMinifyPlugin };
+/* eslint-enable unicorn/prefer-export-from */
