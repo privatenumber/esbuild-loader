6ec4abfdf7e9c4a2610bfe788bac8c06ab09999e
Hiroki Osame <hiroki.osame@gmail.com>
2023-01-20 02:39:42 +0900

    refactor(plugin): functional style

--- src/plugin.ts
+++ src/plugin.ts
@@ -12,214 +12,219 @@ import type { EsbuildPluginOptions } from './types';
 type Compiler = webpack4.Compiler | webpack5.Compiler;
 type Compilation = webpack4.compilation.Compilation | webpack5.Compilation;
 type Asset = webpack4.compilation.Asset | Readonly<webpack5.Asset>;
+type EsbuildTransform = typeof defaultEsbuildTransform;
 
 const isJsFile = /\.[cm]?js(?:\?.*)?$/i;
 const isCssFile = /\.css(?:\?.*)?$/i;
-const pluginName = 'esbuild-minify';
+const pluginName = 'EsbuildPlugin';
 
-const granularMinifyConfigs = ['minifyIdentifiers', 'minifySyntax', 'minifyWhitespace'] as const;
-class EsbuildPlugin {
-	private readonly options: EsbuildPluginOptions;
+const transformAssets = async (
+	options: EsbuildPluginOptions,
+	transform: EsbuildTransform,
+	compilation: Compilation,
+	useSourceMap: boolean,
+): Promise<void> => {
+	const { compiler } = compilation;
+	const sources = 'webpack' in compiler && compiler.webpack.sources;
+	const SourceMapSource = (sources ? sources.SourceMapSource : WP4SourceMapSource);
+	const RawSource = (sources ? sources.RawSource : WP4RawSource);
 
-	private readonly transform: typeof defaultEsbuildTransform;
+	const {
+		css: minifyCss,
+		include,
+		exclude,
+		...transformOptions
+	} = options;
 
-	constructor(options: EsbuildPluginOptions = {}) {
-		const {
-			implementation,
-			...remainingOptions
-		} = options;
+	const assets = (compilation.getAssets() as Asset[]).filter(asset => (
 
-		if (implementation && typeof implementation.transform !== 'function') {
-			throw new TypeError(
-				`[${EsbuildPlugin.name}] implementation.transform must be an esbuild transform function. Received ${typeof implementation.transform}`,
-			);
+		// Filter out already minimized
+		!asset.info.minimized
+
+		// Filter out by file type
+		&& (
+			isJsFile.test(asset.name)
+			|| (minifyCss && isCssFile.test(asset.name))
+		)
+		&& matchObject(
+			{ include, exclude },
+			asset.name,
+		)
+	));
+
+	await Promise.all(assets.map(async (asset) => {
+		const assetIsCss = isCssFile.test(asset.name);
+		let source: string | Buffer | ArrayBuffer;
+		let map = null;
+
+		if (asset.source.sourceAndMap) {
+			const sourceAndMap = asset.source.sourceAndMap();
+			source = sourceAndMap.source;
+			map = sourceAndMap.map;
+		} else {
+			source = asset.source.source();
+			if (asset.source.map) {
+				map = asset.source.map();
+			}
 		}
 
-		this.transform = implementation?.transform ?? defaultEsbuildTransform;
-
-		const hasGranularMinificationConfig = granularMinifyConfigs.some(
-			minifyConfig => minifyConfig in options,
-		);
-
-		if (!hasGranularMinificationConfig) {
-			remainingOptions.minify = true;
-		}
-
-		this.options = remainingOptions;
-	}
-
-	apply(compiler: Compiler): void {
-		const { options } = this;
-		const meta = JSON.stringify({
-			name: 'esbuild-loader',
-			version,
-			options,
+		const sourceAsString = source.toString();
+		const result = await transform(sourceAsString, {
+			...transformOptions,
+			loader: (
+				assetIsCss
+					? 'css'
+					: transformOptions.loader
+			),
+			sourcemap: useSourceMap,
+			sourcefile: asset.name,
 		});
 
-		if (!('format' in options)) {
-			const { target } = compiler.options;
-			const isWebTarget = (
-				Array.isArray(target)
-					? target.includes('web')
-					: target === 'web'
+		if (result.legalComments) {
+			compilation.emitAsset(
+				`${asset.name}.LEGAL.txt`,
+				new RawSource(result.legalComments) as any,
 			);
-			const wontGenerateHelpers = !options.target || (
-				Array.isArray(options.target)
-					? (
-						options.target.length === 1
-						&& options.target[0] === 'esnext'
+		}
+
+		compilation.updateAsset(
+			asset.name,
+			(
+				result.map
+					? new SourceMapSource(
+						result.code,
+						asset.name,
+						result.map as any,
+						sourceAsString,
+						map as any,
+						true,
 					)
-					: options.target === 'esnext'
-			);
+					: new RawSource(result.code)
+			) as any,
+			{
+				...asset.info,
+				minimized: true,
+			},
+		);
+	}));
+};
 
-			if (isWebTarget && !wontGenerateHelpers) {
-				options.format = 'iife';
-			}
-		}
-
-		compiler.hooks.compilation.tap(pluginName, (compilation) => {
-			compilation.hooks.chunkHash.tap(pluginName, (_, hash) => hash.update(meta));
-
-			/**
-			 * Check if sourcemaps are enabled
-			 * Webpack 4: https://github.com/webpack/webpack/blob/v4.46.0/lib/SourceMapDevToolModuleOptionsPlugin.js#L20
-			 * Webpack 5: https://github.com/webpack/webpack/blob/v5.75.0/lib/SourceMapDevToolModuleOptionsPlugin.js#LL27
-			 */
-			let useSourceMap = false;
-			compilation.hooks.finishModules.tap(
-				pluginName,
-				(modules) => {
-					const firstModule = (
-						Array.isArray(modules)
-							? modules[0]
-							: (modules as Set<webpack5.Module>).values().next().value as webpack5.Module
-					);
-					useSourceMap = firstModule.useSourceMap;
-				},
-			);
-
-			if ('processAssets' in compilation.hooks) {
-				compilation.hooks.processAssets.tapPromise(
-					{
-						name: pluginName,
-						// @ts-expect-error undefined on Function type
-						stage: compilation.constructor.PROCESS_ASSETS_STAGE_OPTIMIZE_SIZE,
-						additionalAssets: true,
-					},
-					async () => await this.transformAssets(compilation, useSourceMap),
-				);
-
-				compilation.hooks.statsPrinter.tap(pluginName, (statsPrinter) => {
-					statsPrinter.hooks.print
-						.for('asset.info.minimized')
-						.tap(
-							pluginName,
-							(
-								minimized,
-								{ green, formatFlag },
-							// @ts-expect-error type incorrectly doesn't accept undefined
-							) => (
-								minimized
-									// @ts-expect-error type incorrectly doesn't accept undefined
-									? green(formatFlag('minimized'))
-									: undefined
-							),
-						);
-				});
-			} else {
-				compilation.hooks.optimizeChunkAssets.tapPromise(
-					pluginName,
-					async () => await this.transformAssets(compilation, useSourceMap),
-				);
-			}
-		});
+export default function EsbuildPlugin(
+	{
+		implementation,
+		...options
+	}: EsbuildPluginOptions = {},
+) {
+	if (
+		implementation
+		&& typeof implementation.transform !== 'function'
+	) {
+		throw new TypeError(
+			`[${pluginName}] implementation.transform must be an esbuild transform function. Received ${typeof implementation.transform}`,
+		);
 	}
 
-	private async transformAssets(
-		compilation: Compilation,
-		useSourceMap: boolean,
-	): Promise<void> {
-		const { compiler } = compilation;
-		const sources = 'webpack' in compiler && compiler.webpack.sources;
-		const SourceMapSource = (sources ? sources.SourceMapSource : WP4SourceMapSource);
-		const RawSource = (sources ? sources.RawSource : WP4RawSource);
+	const transform = implementation?.transform ?? defaultEsbuildTransform;
 
-		const {
-			css: minifyCss,
-			include,
-			exclude,
-			...transformOptions
-		} = this.options;
+	const hasGranularMinificationConfig = (
+		'minifyIdentifiers' in options
+		|| 'minifySyntax' in options
+		|| 'minifyWhitespace' in options
+	);
 
-		const assets = (compilation.getAssets() as Asset[]).filter(asset => (
+	if (!hasGranularMinificationConfig) {
+		options.minify = true;
+	}
 
-			// Filter out already minimized
-			!asset.info.minimized
+	return {
+		apply(compiler: Compiler): void {
+			if (!('format' in options)) {
+				const { target } = compiler.options;
+				const isWebTarget = (
+					Array.isArray(target)
+						? target.includes('web')
+						: target === 'web'
+				);
+				const wontGenerateHelpers = !options.target || (
+					Array.isArray(options.target)
+						? (
+							options.target.length === 1
+							&& options.target[0] === 'esnext'
+						)
+						: options.target === 'esnext'
+				);
 
-			// Filter out by file type
-			&& (
-				isJsFile.test(asset.name)
-				|| (minifyCss && isCssFile.test(asset.name))
-			)
-			&& matchObject({ include, exclude }, asset.name)
-		));
-
-		await Promise.all(assets.map(async (asset) => {
-			const assetIsCss = isCssFile.test(asset.name);
-			let source: string | Buffer | ArrayBuffer;
-			let map = null;
-
-			if (asset.source.sourceAndMap) {
-				const sourceAndMap = asset.source.sourceAndMap();
-				source = sourceAndMap.source;
-				map = sourceAndMap.map;
-			} else {
-				source = asset.source.source();
-				if (asset.source.map) {
-					map = asset.source.map();
+				if (isWebTarget && !wontGenerateHelpers) {
+					options.format = 'iife';
 				}
 			}
 
-			const sourceAsString = source.toString();
-			const result = await this.transform(sourceAsString, {
-				...transformOptions,
-				loader: (
-					assetIsCss
-						? 'css'
-						: transformOptions.loader
-				),
-				sourcemap: useSourceMap,
-				sourcefile: asset.name,
-			});
+			compiler.hooks.compilation.tap(pluginName, (compilation) => {
+				const meta = JSON.stringify({
+					name: 'esbuild-loader',
+					version,
+					options,
+				});
 
-			if (result.legalComments) {
-				compilation.emitAsset(
-					`${asset.name}.LEGAL.txt`,
-					new RawSource(result.legalComments) as any,
+				compilation.hooks.chunkHash.tap(
+					pluginName,
+					(_, hash) => hash.update(meta),
 				);
-			}
 
-			compilation.updateAsset(
-				asset.name,
-				(
-					result.map
-						? new SourceMapSource(
-							result.code,
-							asset.name,
-							result.map as any,
-							sourceAsString,
-							map as any,
-							true,
-						)
-						: new RawSource(result.code)
-				) as any,
-				{
-					...asset.info,
-					minimized: true,
-				},
-			);
-		}));
-	}
+				/**
+				 * Check if sourcemaps are enabled
+				 * Webpack 4: https://github.com/webpack/webpack/blob/v4.46.0/lib/SourceMapDevToolModuleOptionsPlugin.js#L20
+				 * Webpack 5: https://github.com/webpack/webpack/blob/v5.75.0/lib/SourceMapDevToolModuleOptionsPlugin.js#LL27
+				 */
+				let useSourceMap = false;
+				compilation.hooks.finishModules.tap(
+					pluginName,
+					(modules) => {
+						const firstModule = (
+							Array.isArray(modules)
+								? modules[0]
+								: (modules as Set<webpack5.Module>).values().next().value as webpack5.Module
+						);
+						useSourceMap = firstModule.useSourceMap;
+					},
+				);
+
+				// Webpack 5
+				if ('processAssets' in compilation.hooks) {
+					compilation.hooks.processAssets.tapPromise(
+						{
+							name: pluginName,
+							// @ts-expect-error undefined on Function type
+							stage: compilation.constructor.PROCESS_ASSETS_STAGE_OPTIMIZE_SIZE,
+							additionalAssets: true,
+						},
+						() => transformAssets(options, transform, compilation, useSourceMap),
+					);
+
+					compilation.hooks.statsPrinter.tap(pluginName, (statsPrinter) => {
+						statsPrinter.hooks.print
+							.for('asset.info.minimized')
+							.tap(
+								pluginName,
+								(
+									minimized,
+									{ green, formatFlag },
+								// @ts-expect-error type incorrectly doesn't accept undefined
+								) => (
+									minimized
+										// @ts-expect-error type incorrectly doesn't accept undefined
+										? green(formatFlag('minimized'))
+										: undefined
+								),
+							);
+					});
+				} else {
+					compilation.hooks.optimizeChunkAssets.tapPromise(
+						pluginName,
+						() => transformAssets(options, transform, compilation, useSourceMap),
+					);
+				}
+			});
+		},
+	};
 }
-
-export default EsbuildPlugin;
