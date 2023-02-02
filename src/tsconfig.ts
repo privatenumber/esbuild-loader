1eeb2f5aa81f62a8ff3d660107ee3eecf69ef58d
hiroki osame <hiroki.osame@gmail.com>
2023-02-01 23:04:26 -0500

    fix: apply tsconfig only to matching files (#310)
    BREAKING CHANGE: `tsconfig.json` is now only applied to files it matches (via `include`/`exclude`/`files`)

--- src/tsconfig.ts
+++ src/tsconfig.ts
@@ -1,6 +1,11 @@
-import { getTsconfig } from 'get-tsconfig';
+import {
+	getTsconfig,
+	createFilesMatcher,
+} from 'get-tsconfig';
 import type { TransformOptions } from 'esbuild';
 
 const foundTsconfig = getTsconfig();
 
 export const tsconfig = foundTsconfig?.config as (TransformOptions['tsconfigRaw'] | undefined);
+
+export const fileMatcher = foundTsconfig && createFilesMatcher(foundTsconfig);
